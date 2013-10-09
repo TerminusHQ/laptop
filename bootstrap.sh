2a2d80fe804bde5d7f040c9146cb3941a0fd7799
Saito <saitowu@gmail.com>
2013-10-09 10:56:03 +0800

    fix zsh read function

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 if ! [ -e "$HOME/.ssh/id_rsa.pub" ]
 then
   echo "Generating ssh key..."
-  read -p "Please enter the email you want to associate with your ssh key: " email
+  echo "Please enter the email you want to associate with your ssh key: \c"
+  read email
   ssh-keygen -t rsa -C "$email"
 fi
 
